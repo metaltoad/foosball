232871021d193eab9d08260d48fd9e15c1a70b0c
Peter Bliss <pbliss@metaltoad.com>
2014-11-29 19:19:51 -0800

    removed unneeded reset key

--- app/scripts/controllers/InputCtrl.js
+++ app/scripts/controllers/InputCtrl.js
@@ -4,18 +4,7 @@ app.controller('InputCtrl', function($scope, $route, $rootScope) {
 
 $scope.recordInput = function($e) {
 
-  var resetKey = "w";
-
   var key = String.fromCharCode($e.which);
-
-  //some keys are universal so allow them to be used here
-  switch(key) {
-    case resetKey: {
-        //send back to the login
-        $location.url("/");
-        break;
-      }
-  }
 
   switch($route.current.originalPath) {
     case "/vs": {
