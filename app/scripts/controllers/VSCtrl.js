34ac3ddc53cdad00b92b7f83e90b48dee62ca066
Peter Bliss <pbliss@metaltoad.com>
2014-11-29 19:20:55 -0800

    re enabled the vs timeout

--- app/scripts/controllers/VSCtrl.js
+++ app/scripts/controllers/VSCtrl.js
@@ -2,7 +2,7 @@
 
 app.controller('VSCtrl', function($scope, $routeParams, $rootScope, $timeout, $location) {
 
-  //$timeout(function() { $location.url("/game");}, 3000);
+  $timeout(function() { $location.url("/game");}, 3000);
 
   var randLoc = Math.floor((Math.random() * Object.keys($rootScope.themedata.backgrounds).length));
   $rootScope.location = $rootScope.themedata.backgrounds[randLoc];
